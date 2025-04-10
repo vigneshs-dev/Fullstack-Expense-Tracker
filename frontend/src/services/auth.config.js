997b4d59a0238452365a99da21c31b0d21eaae94
vigneshs <vigneshs@melss.com>
2025-04-10 10:20:23 +0530

    Added Dockerfiles for frontend and backend and docker-compose.yml file to run all these 3 Docker containers - Bug

--- frontend/src/services/auth.config.js
+++ frontend/src/services/auth.config.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = "http://localhost:8080/mywallet";
+// const API_BASE_URL = "http://localhost:8080/mywallet";
+
+// For Docker
+const API_BASE_URL = "http://backend:8080/mywallet";
 
 export default API_BASE_URL;
